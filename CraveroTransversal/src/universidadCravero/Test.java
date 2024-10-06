1c95ce4656f5d9398ee8bf871d96bd7c2e70d764
Cris153 <cristiancravero023@gmail.com>
2024-10-06 20:39:19 -0300

    Reorganizando proyecto

--- CraveroTransversal/src/universidadCravero/Test.java
+++ CraveroTransversal/src/universidadCravero/Test.java
@@ -3,12 +3,19 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package AccesoADatos;
+package universidadCravero;
 
 /**
  *
  * @author crist
  */
-public class Conexion {
+public class Test {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
     
 }
--- CraveroTransversal/src/universidadCravero/entidades/Alumno.java
+++ CraveroTransversal/src/universidadCravero/entidades/Alumno.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Entidades;
+package universidadCravero.entidades;
 
 /**
  *
  * @author crist
  */
-public class Materia {
+public class Alumno {
     
 }
