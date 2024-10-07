1d3da4d960762d3714c7dfe9b37f5ce2e85c9dc8
Cris153 <cristiancravero023@gmail.com>
2024-10-06 22:45:49 -0300

    1era Entrega transversal terminada

--- CraveroTransversal/src/universidadCravero/Test.java
+++ CraveroTransversal/src/universidadCravero/Test.java
@@ -7,6 +7,7 @@ package universidadCravero;
 
 import java.sql.Connection;
 import java.time.LocalDate;
+import java.time.Month;
 import universidadCravero.accesoADatos.AlumnoData;
 import universidadCravero.accesoADatos.Conexion;
 import universidadCravero.entidades.Alumno;
@@ -22,15 +23,68 @@ public class Test {
      */
     public static void main(String[] args) {
         
-        Alumno juan= new Alumno(456456457, "Martinez", "Oscar", LocalDate.of(1994, 4,12), true);
+        Alumno uno = new Alumno(43765287, "Cravero", "Cristian", LocalDate.of(2001, 10, 15), true);
+        Alumno dos = new Alumno(40884234, "Perez", "Pablo", LocalDate.of(1997, 12, 24), true);
+        Alumno tre = new Alumno(45264265, "Raposo", "Martin", LocalDate.of(2004, 2, 1), true);
+        Alumno cuatro = new Alumno(42005001, "Martinez", "Lautaro", LocalDate.of(1999, 7, 31), true);
         
         AlumnoData al = new AlumnoData();
         
+        
+        System.out.println("Cargando alumnos");
+        
+        al.cargarAlumno(uno);
+        al.cargarAlumno(dos);
+        al.cargarAlumno(tre);
+        al.cargarAlumno(cuatro);
+        
+        //SE CARGAN ALUMNOS Y SE LISTAN
+        
+        
+        
+        System.out.println("Listando Alumnos");
+        
         for(Alumno alumno: al.listar()){
-            System.out.println(alumno.getDni());
-            System.out.println(alumno.getApellido());
-            System.out.println(alumno.getNombre());
+            System.out.println("---------");
+            System.out.println("DNI: " + alumno.getDni());
+            System.out.println("Apellido: " + alumno.getApellido());
+            System.out.println("Nombre: " + alumno.getNombre());
+            System.out.println("Fecha Nacimiento: " + alumno.getFecha_nac());
         }
+        System.out.println("Eliminando alumno...");
+        al.eliminarAlumno(45264265);
+        
+        //SE ELIMINA UNO Y SE LISTAN ALUMNOS
+        
+        System.out.println("Listando Alumnos");
+        
+        
+        for(Alumno alumno: al.listar()){
+            System.out.println("DNI: " + alumno.getDni());
+            System.out.println("Apellido: " + alumno.getApellido());
+            System.out.println("Nombre: " + alumno.getNombre());
+            System.out.println("Fecha Nacimiento: " + alumno.getFecha_nac());
+        }
+        
+        
+        //MOSTRAR ALUMNO POR DNI
+        
+//        Alumno alDni = al.buscarAlumnoPorDni(0);
+//        
+//        System.out.println("DNI: " + alDni.getDni());
+//        System.out.println("Apellido: " + alDni.getApellido());
+//        System.out.println("Nombre: " + alDni.getNombre());
+//        System.out.println("Fecha Nacimiento: " + alDni.getFecha_nac());
+        
+        
+        //MOSTRAR ALUMNO POR ID
+        
+//        Alumno alId = al.buscarAlumnoPorId(4);
+//        
+//        System.out.println("DNI: " + alId.getDni());
+//        System.out.println("Apellido: " + alId.getApellido());
+//        System.out.println("Nombre: " + alId.getNombre());
+//        System.out.println("Fecha Nacimiento: " + alId.getFecha_nac());
     }
     
 }
